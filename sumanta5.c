7e4067e10228bce2d9fd5ceb513f55debea3927f
SumantaBhattacharya <sumanta2004@gmail.com>
2025-05-06 00:59:12 +0530

    Add C program to display sizes of various data types

---
+++ sumanta5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    //to find out the bite or size of data type
+    int ;
+    char ;
+    float;
+    double ;
+
+    
+    printf("size of integer:%d\n ",sizeof(int));
+printf("size of character data type:%c\n ",sizeof (char) );
+     printf("size of floating data type:%f\n ",sizeof(float ));
+printf("size of double floating point data type:%lf\n ",sizeof( double));
+
+    return 0;
+
+}
