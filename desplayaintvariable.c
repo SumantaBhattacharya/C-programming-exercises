90e0f3cddafc1ea717a92a33647587b617b08079
SumantaBhattacharya <sumanta2004@gmail.com>
2025-05-05 13:49:33 +0530

    Add C program to display integer, float, and double variables

---
+++ desplayaintvariable.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int r;
+ float o;
+    double p;
+    
+ r=  3;
+ o=  8;
+  p=  9;
+    printf(":%d\n",r);
+    printf(":%f\n",o);
+       printf(":%lf\n",p); 
+return 0;
+}
