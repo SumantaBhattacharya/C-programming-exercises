60328915b34e94f60bd6fb6c4dd32254f2a5d712
SumantaBhattacharya <sumanta2004@gmail.com>
2025-05-12 16:23:48 +0530

    Add C program to calculate the area and perimeter of a circle

---
+++ sumanta4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int r;
+
+    
+    printf("enter the radius of a circle");
+    scanf("%d",&r);
+int area=  3.14*r*r;
+int parameter=  2*3.14*r;
+     printf("the area of a circle  :%d\n",area);
+    printf("the parameter of a circle  :%d\n",parameter);
+
+    
+return 0;
+}
