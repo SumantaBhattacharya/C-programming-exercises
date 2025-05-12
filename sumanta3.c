1806569fe9bfd78292738b4f0182a0ee941f7fff
SumantaBhattacharya <sumanta2004@gmail.com>
2025-05-12 16:23:39 +0530

    Add C program to perform basic arithmetic operations on two numbers

---
+++ sumanta3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+
+    printf("enter the first number:");
+    scanf ("%d",&a);
+    printf("enter the second number:");
+    scanf("%d", &b);
+
+    printf("the sum of these two number is :%d\n",a+b);
+     printf("the SUB of these two number is :%d\n",a-b);
+      printf("the MOD of these two number is :%d\n",a%b);
+       printf("the MULTIPLICATION of these two number is :%d\n",a*b);
+       printf("the devision of these two number is :%d\n",a/b);
+
+    return 0;
+}
