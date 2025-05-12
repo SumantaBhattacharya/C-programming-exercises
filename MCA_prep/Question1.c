86d706ba68dc2929aab96eff235029a357d18877
SumantaBhattacharya <sumanta2004@gmail.com>
2025-05-12 16:25:43 +0530

    Add C program to demonstrate variable shadowing and output

---
+++ MCA_prep/Question1.c
@@ -0,0 +1,11 @@
+// what is the output of this program?
+
+#include <stdio.h>
+int main() {
+    int main = 3;
+
+    printf("%d", main);
+    return 0;
+    // syntax error falls in compile time error, runtime error, logical error
+}
+
