a04ecaac88d11421106e0128c529e73308ec8bd5
SumantaBhattacharya <sumanta2004@gmail.com>
2025-05-12 16:25:55 +0530

    Add C program to demonstrate printf behavior and variable naming rules

---
+++ MCA_prep/Question2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main() {
+    
+    // what is the output of this program?
+    // printf("variable! %d", x)
+    // return 0;
+    // compile time error
+
+
+    // Output of the statement is
+    // %d is format specifier for int
+    printf("%d", printf("hello"));// does the execution from right to left thats why hello will come first then the length of the string
+
+    /*
+    4. Which of the following is true for a variable names in C?
+       A. Variable names cannot be start with a digit
+       B. Variable can be of any length
+       C. They can contain alphanumeric characters as well as special characters
+       D. Reserved word can be used as variable name
+
+       ans- Variable names cannot be start with a digit
+
+    5. How many keywords are there in C - 32
+
+    6. Which of the following cannot be a variable name in C?
+       A. TRUE
+       B. friend
+       C. export
+       D. volatile
+
+       ans- D. volatile(volatile indicates a variable can be changes at any time)    
+
+    */
+
+}
