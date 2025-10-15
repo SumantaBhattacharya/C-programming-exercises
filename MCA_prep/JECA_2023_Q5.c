9f4d2dabd7d715a2ba3ca84fc017c150915d54c2
Sumanta Bhattacharya <148477304+SumantaBhattacharya@users.noreply.github.com>
2025-10-15 10:33:48 +0530

    JECA_2023_Q5.c
    JECA 2023 Q5

---
+++ MCA_prep/JECA_2023_Q5.c
@@ -0,0 +1,15 @@
+// ## 5. What is the output of the following program ? 
+
+#include <stdio.h> 
+enum colors{RED, BROWN, ORANGE}; 
+void main( ) 
+{ 
+ printf (“%ld..%f..%d”, RED, BROWN, ORANGE); 
+} 
+
+/*
+(A) Compiler Error (B) 0..0.000000..1 
+(C) 0..1.000000..2 (D) Runtime Error
+*/
+
+// (A) Compiler Error
